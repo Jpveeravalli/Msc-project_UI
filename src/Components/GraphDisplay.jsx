5c6722ce5f498baa122f61fbc1d29173c4a85d9c
N1016510 <anudeep.jami@gmail.com>
2023-01-19 21:19:54 +0000

    hi jp

--- src/Components/GraphDisplay.jsx
+++ src/Components/GraphDisplay.jsx
@@ -1,11 +1,117 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import form from "react-bootstrap";
+import { Container, Form, Row, Col, Card } from "react-bootstrap";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, BarElement, Title,CategoryScale,LinearScale } from "chart.js";
+import { Doughnut, Bar } from "react-chartjs-2";
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title);
 
 export default function InputForm() {
+
+    const data = {
+        title: 'Mortality',
+        labels: [
+            'Mortality'
+        ],
+        datasets: [{
+            data: [45,55],
+            backgroundColor: [
+                'blue',
+                '#808080'
+            ],
+            hoverOffset: 4
+        }]
+    };
+
+    const data2 = {
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Bar Chart',
+          },
+        },
+      };
+
     return (
-       
-        <h1>This is GraphDisplay page</h1>
+        <>
+            <h1>Graphs</h1>
+            <br></br>
+            <Container>
+                <Card>
+                    <br></br>
+                    <Form>
+                        <Form.Group as={Row}>
+                            <Col sm={2}>
+                                <Form.Check
+                                    type="radio"
+                                    label="1"
+                                    name="1"
+                                    id="1"
+                                />
+                            </Col>
+                            <Col sm={2}>
+                                <Form.Check
+                                    type="radio"
+                                    label="2"
+                                    name="2"
+                                    id="2"
+                                />
+                            </Col>
+                            <Col sm={2}>
+                                <Form.Check
+                                    type="radio"
+                                    label="3"
+                                    name="3"
+                                    id="3"
+                                />
+                            </Col>
+                            <Col sm={2}>
+                                <Form.Check
+                                    type="radio"
+                                    label="4"
+                                    name="4"
+                                    id="4"
+                                />
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                    <div as={Row} style={{ height: '25rem', width: '25rem' }}>
+                        <Doughnut data={data} id='1'/>
+                    </div>
+                    <div as={Row}>
+                        <Bar data={data2} id='2' options={options}/>
+                    </div>
+                </Card>
+            </Container>
+        </>
 
     );
-    }
+}
--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,60 +1,195 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Card,Row, Col,Container,Form} from "react-bootstrap";
+import { Card, Row, Col, Container, Form } from "react-bootstrap";
 
 export default function InputForm() {
-    return (  
+    return (
         //create input form with react-bootstrap library
         //crate a form group with label and input field on same line
         <>
-        
-        <Container>
-            <Card>
-                <Form>
-                    <Form.Group as={Row}>
-                        <Form.Label column sm={3}>Year of birth</Form.Label>
-                        <Col sm={8}>
-                            <Form.Control type="number" placeholder="Enter year of birth" />        
-                        </Col>                        
-                    </Form.Group>
-                    <Form.Group as={Row}>
-                        <Form.Label column sm={3}>Gestation Weeks</Form.Label>
-                        <Col sm={8}>
-                            <Form.Control type="number" placeholder="Enter Gestation weeks" />        
-                        </Col>                        
-                    </Form.Group>
-                    <Form.Group as={Row}>
-                        <Form.Label column sm={2}>Z-score</Form.Label>
-                        <Col sm={8}>
-                            <Form.Control type="number" placeholder="Enter Zscore" />        
-                        </Col>                        
-                    </Form.Group>
-                    <fieldset>
-                    <Form.Group as={Row}>
-                        <Form.Label as="legend" column sm={2}>Gender</Form.Label>
-                        <Col sm={5}>
-                            <Form.Check
-                            type="radio"
-                            label="Male"
-                            name="gender"
-                            id="radioMale"
-                            />
+
+            <Container>
+                <h1>Data</h1>
+                <br></br>
+                <Card>
+                    <br></br>
+                    <Form>
+                        <Form.Group as={Row}>
+                            <Form.Label column sm={3}>Year of birth</Form.Label>
+                            <Col sm={8}>
+                                <Form.Control type="number" placeholder="Enter year of birth" />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row}>
+                            <Form.Label column sm={3}>Gestation Weeks</Form.Label>
+                            <Col sm={8}>
+                                <Form.Control type="number" placeholder="Enter Gestation weeks" />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row}>
+                            <Form.Label column sm={2}>Z-score</Form.Label>
+                            <Col sm={8}>
+                                <Form.Control type="number" placeholder="Enter Zscore" />
+                            </Col>
+                        </Form.Group>
+                        {/* create a form group for Gender */}
+                        <Form.Group as={Row}>
+                            <Form.Label as="legend" column sm={2}>Gender</Form.Label>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="Male"
+                                    name="gender"
+                                    id="radioMale"
+                                />
                             </Col>
                             <Col sm={5}>
-                            <Form.Check
-                            type="radio"
-                            label="Female"
-                            name="gender"
-                            id="radioFemale"
-                            />
-                        </Col>
-                    </Form.Group>
-                </fieldset>
+                                <Form.Check
+                                    type="radio"
+                                    label="Female"
+                                    name="gender"
+                                    id="radioFemale"
+                                />
+                            </Col>
+                        </Form.Group>
+                        {/* create a form group for Level 3 */}
+                        <Form.Group as={Row}>
+                            <Form.Label as="legend" column sm={2}>Level 3</Form.Label>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="Yes"
+                                    name="level3"
+                                    id="level3Yes"
+                                />
+                            </Col>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="No"
+                                    name="level3"
+                                    id="level3No"
+                                    defaultChecked
+                                />
+                            </Col>
+                        </Form.Group>
+                        {/* create a form group for PROM*/}
+                        <Form.Group as={Row}>
+                            <Form.Label as="legend" column sm={2}>PROM</Form.Label>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="Yes"
+                                    name="prom"
+                                    id="promYes"
+                                />
+                            </Col>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="No"
+                                    name="prom"
+                                    id="promNo"
+                                    defaultChecked
+                                />
+                            </Col>
+                        </Form.Group>
+                        {/* create a form group for Chorioamnionitis */}
+                        <Form.Group as={Row}>
+                            <Form.Label as="legend" column sm={3}>Chorioamnionitis</Form.Label>
+                            <Col sm={4}>
+                                <Form.Check
+                                    type="radio"
+                                    label="Yes"
+                                    name="chorioamnionitis"
+                                    id="chorioamnionitisYes"
+                                />
+                            </Col>
+                            <Col sm={4}>
+                                <Form.Check
+                                    type="radio"
+                                    label="No"
+                                    name="chorioamnionitis"
+                                    id="chorioamnionitisNo"
+                                    defaultChecked
+                                />
+                            </Col>
+                        </Form.Group>
+                        {/* create a form group for Maternal Antenatal Steroids*/}
+                        <Form.Group as={Row}>
+                            <Form.Label as="legend" column sm={2}>Maternal Antenatal Steroids</Form.Label>
+                            <Col sm={3}>
+                                <Form.Check
+                                    type="radio"
+                                    label="No Treatment"
+                                    name="MaternalAntenatalSteroids"
+                                    id="MaternalAntenatalSteroidsNT"
+                                />
+                            </Col>
+                            <Col sm={3}>
+                                <Form.Check
+                                    type="radio"
+                                    label="Incomplete Treatment"
+                                    name="MaternalAntenatalSteroids"
+                                    id="MaternalAntenatalSteroidsIT"
+                                />
+                            </Col>
+                            <Col sm={3}>
+                                <Form.Check
+                                    type="radio"
+                                    label="Completed Treatment"
+                                    name="MaternalAntenatalSteroids"
+                                    id="MaternalAntenatalSteroidsCT"
+                                    defaultChecked
+                                />
+                            </Col>
+                        </Form.Group>
+                        {/* create a form group for Congenital Anomaly  */}
+                        <Form.Group as={Row}>
+                            <Form.Label as="legend" column sm={2}>Congenital Anomaly</Form.Label>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="Yes"
+                                    name="CongenitalAnomaly"
+                                    id="CongenitalAnomalyYes"
+                                />
+                            </Col>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="No"
+                                    name="CongenitalAnomaly"
+                                    id="CongenitalAnomalyNo"
+                                    defaultChecked
+                                />
+                            </Col>
+                        </Form.Group>
+                        {/* create a form group for Multiple Births */}
+                        <Form.Group as={Row}>
+                            <Form.Label as="legend" column sm={2}>Multiple Births</Form.Label>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="Yes"
+                                    name="MultipleBirths"
+                                    id="MultipleBirthsYes"
+                                />
+                            </Col>
+                            <Col sm={5}>
+                                <Form.Check
+                                    type="radio"
+                                    label="No"
+                                    name="MultipleBirths"
+                                    id="MultipleBirthsNo"
+                                    defaultChecked
+                                />
+                            </Col>
+                        </Form.Group>
 
-                </Form>
-            </Card>
-
-        </Container>       
+                    </Form>
+                </Card>
+            </Container>
         </>
     );
 }
